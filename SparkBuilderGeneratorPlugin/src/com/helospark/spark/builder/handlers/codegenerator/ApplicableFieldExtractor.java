29e35e0086fd377c2f10be6e7c6a4bd2c539ccb7
black <bcsababcsaba9021@gmail.com>
2016-12-03 22:27:21 +0100

    Exclude static fields from generated builder

--- SparkBuilderGeneratorPlugin/src/com/helospark/spark/builder/handlers/codegenerator/ApplicableFieldExtractor.java
+++ SparkBuilderGeneratorPlugin/src/com/helospark/spark/builder/handlers/codegenerator/ApplicableFieldExtractor.java
@@ -2,8 +2,11 @@ package com.helospark.spark.builder.handlers.codegenerator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.Modifier;
+import org.eclipse.jdt.core.dom.Modifier.ModifierKeyword;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 
 import com.helospark.spark.builder.handlers.codegenerator.domain.NamedVariableDeclarationField;
@@ -15,15 +18,34 @@ import com.helospark.spark.builder.handlers.codegenerator.domain.NamedVariableDe
  */
 public class ApplicableFieldExtractor {
 
-	public List<NamedVariableDeclarationField> filterApplicableFields(FieldDeclaration[] fields) {
-		List<NamedVariableDeclarationField> namedVariableDeclarations = new ArrayList<>();
-		for (FieldDeclaration field : fields) {
-			Object o = field.fragments().get(0);
-			if (o instanceof VariableDeclarationFragment) {
-				String fieldName = ((VariableDeclarationFragment) o).getName().toString();
-				namedVariableDeclarations.add(new NamedVariableDeclarationField(field, fieldName));
-			}
-		}
-		return namedVariableDeclarations;
-	}
+    @SuppressWarnings("unchecked")
+    public List<NamedVariableDeclarationField> filterApplicableFields(FieldDeclaration[] fields) {
+        List<NamedVariableDeclarationField> namedVariableDeclarations = new ArrayList<>();
+        for (FieldDeclaration field : fields) {
+            Object o = field.fragments().get(0);
+            List<VariableDeclarationFragment> fragments = field.fragments();
+            namedVariableDeclarations.addAll(getFilteredDeclarations(field, fragments));
+        }
+        return namedVariableDeclarations;
+    }
+
+    private List<NamedVariableDeclarationField> getFilteredDeclarations(FieldDeclaration field, List<VariableDeclarationFragment> fragments) {
+        return fragments.stream()
+                .filter(variableFragment -> !isStatic(field))
+                .map(variableFragment -> createNamedVariableDeclarations(variableFragment, field))
+                .collect(Collectors.toList());
+    }
+
+    private NamedVariableDeclarationField createNamedVariableDeclarations(VariableDeclarationFragment variableDeclarationFragment, FieldDeclaration fieldDeclaration) {
+        String fieldName = variableDeclarationFragment.getName().toString();
+        return new NamedVariableDeclarationField(fieldDeclaration, fieldName);
+    }
+
+    private boolean isStatic(FieldDeclaration field) {
+        List<Modifier> fieldModifiers = field.modifiers();
+        return fieldModifiers.stream()
+                .filter(modifer -> modifer.getKeyword().equals(ModifierKeyword.STATIC_KEYWORD))
+                .findFirst()
+                .isPresent();
+    }
 }
